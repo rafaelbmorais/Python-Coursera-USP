5e6c5d07fb2a38c43f2ce5bb2b3994ad87037250
rafaelbmorais <morais.rafael@outlook.com>
2022-05-18 11:59:10 -0300

    Primeiro commit do arquivo.

---
+++ Parte2/P2S6_elefantes.py
@@ -0,0 +1,39 @@
+'''
+1 - Implemente a função incomodam(n) que devolve uma string contendo "incomodam "
+(a palavra seguida de um espaço) n vezes. Se n não for um inteiro estritamente
+positivo, a função deve devolver uma string vazia. Essa função deve ser
+implementada utilizando recursão.
+
+2 - Utilizando a função acima, implemente a função elefantes(n) que devolve uma
+string contendo a letra da música "Um elefante incomoda muita gente" de 1 até n
+elefantes. Se n não for maior que 1, a função deve devolver uma string vazia.
+Essa função também deve ser implementada utilizando recursão.
+'''
+
+def incomodam(n):
+
+    if n <= 0:
+        return ""
+    else:
+        return "incomodam " + incomodam(n-1)
+
+#print(incomodam(5))
+
+def elefantes(n):
+    
+    if n < 1:
+        return ""
+
+    if n == 1:
+        return "Um elefante incomoda muita gente\n"
+
+    else:
+        return elefantes(n-1) + str(n) + " elefantes " + incomodam(n) + "muito mais\n" + str(n) + " elefantes incomodam muita gente\n"
+
+            
+
+#print(elefantes(5))
+
+
+
+        
