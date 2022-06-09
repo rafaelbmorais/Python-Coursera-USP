a8ddf773c139286f5a78c88f2cc3579b15afac8e
rafaelbmorais <morais.rafael@outlook.com>
2022-06-09 12:57:05 -0300

    Nome do arquivo alterado.

--- Parte2/Exercicios-Extras/le_matriz.py
+++ Parte2/Exercicios-Extras/le_matriz.py
@@ -1,3 +1,9 @@
+'''
+Funcao que le do teclado o numero n_linhas de linhas e o numero n_colunas de
+colunas e os elementos de uma matriz de inteiros dimensao n_linha x n_colunas.
+Exercicio 11.1
+'''
+
 
 def leia_matriz():
     n_linhas = int(input('Digite o número de linha(s): '))
@@ -9,10 +15,13 @@ def leia_matriz():
         for j in range(n_colunas):
             c = int(input('Digite o elemento [' + str(i) + '][' + str(j) + ']: '))
             linha.append(c)
-
         matriz.append(linha)
 
     return matriz
 
-a_mat = leia_matriz()
-print(a_mat)
+matriz = leia_matriz()
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz[0])):
+        print(matriz[linha][coluna], end = "  ")
+    print()
+
